478eda7856c1e127ae78dbb7d63aaf8a31a536c2
jvfullam <jvfullam@gmail.com>
2017-03-08 02:01:12 -0500

    Added basic google login functionality

--- app/src/main/java/com/example/jsung721/ronaldmcdonald_prototype1/MainActivity.java
+++ app/src/main/java/com/example/jsung721/ronaldmcdonald_prototype1/MainActivity.java
@@ -5,16 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mUserName;
+
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUserName = "anonymous";
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if(mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        } else {
+            mUserName = mFirebaseUser.getDisplayName();
+        }
+
+        Toast.makeText(getApplicationContext(), "Logged in as: " + mUserName, Toast.LENGTH_LONG).show();
+
         Button ViewP = (Button) findViewById(R.id.ViewProf);
         Button TrackM = (Button) findViewById(R.id.TrackMe);
         Button Sett = (Button) findViewById(R.id.Settings);
