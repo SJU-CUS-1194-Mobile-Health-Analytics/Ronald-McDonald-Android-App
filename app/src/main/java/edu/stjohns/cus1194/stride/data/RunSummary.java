88f6ed1450031bbe99e464c972792f674e64bcdf
jvfullam <jvfullam@gmail.com>
2017-04-28 01:52:35 -0400

    added calories to RunSummary and UserProfile

--- app/src/main/java/edu/stjohns/cus1194/stride/data/RunSummary.java
+++ app/src/main/java/edu/stjohns/cus1194/stride/data/RunSummary.java
@@ -5,35 +5,41 @@ public class RunSummary {
     // Instance Variables
     private long totalTimeElapsed;
     private long totalDistanceRun;
+    private double totalCalories;
 
     // Default Constructor
     public RunSummary() {
         this.totalTimeElapsed = 0;
         this.totalDistanceRun = 0;
+        this.totalCalories = 0;
     }
 
     // Constructor With Arguments
-    public RunSummary(long totalTimeElapsed, long totalDistanceRun) {
+    public RunSummary(long totalTimeElapsed, long totalDistanceRun, double totalCalories) {
         this.totalTimeElapsed = totalTimeElapsed;
         this.totalDistanceRun = totalDistanceRun;
+        this.totalCalories = totalCalories;
     }
 
     // Getters
     public long getTotalTimeElapsed() {
         return totalTimeElapsed;
     }
-
     public long getTotalDistanceRun() {
         return totalDistanceRun;
+    }
+    public double getTotalCalories() {
+        return totalCalories;
     }
 
     // Setters
     public void setTotalTimeElapsed(long totalTimeElapsed) {
         this.totalTimeElapsed = totalTimeElapsed;
     }
-
     public void setTotalDistanceRun(long totalDistanceRun) {
         this.totalDistanceRun = totalDistanceRun;
     }
-
+    public void setTotalCalories(double totalCalories) {
+        this.totalCalories = totalCalories;
+    }
 }
---
+++ app/src/main/java/edu/stjohns/cus1194/stride/utils/CalorieCalculator.java
@@ -0,0 +1,43 @@
+package edu.stjohns.cus1194.stride.utils;
+
+import edu.stjohns.cus1194.stride.data.RunSummary;
+import edu.stjohns.cus1194.stride.data.UserProfile;
+
+public class CalorieCalculator {
+
+    public static double calculateCalories(UserProfile user, RunSummary run) {
+        double calories = 0.0;
+
+        double minutes = run.getTotalTimeElapsed()/(1000*60);
+        double METcalc = minutes/run.getTotalDistanceRun();
+        int METmeasure;
+        if(METcalc <= 4)
+            METmeasure = 23;
+        else if(METcalc <= 5)
+            METmeasure = 21;
+        else if(METcalc <= 6)
+            METmeasure = 17;
+        else if(METcalc <= 7)
+            METmeasure = 13;
+        else if(METcalc <= 8)
+            METmeasure = 12;
+        else if(METcalc <= 9)
+            METmeasure = 11;
+        else if(METcalc <= 10)
+            METmeasure = 10;
+        else if(METcalc <= 11)
+            METmeasure = 9;
+        else if(METcalc <= 12)
+            METmeasure = 8;
+        else if(METcalc <= 13)
+            METmeasure = 7;
+        else if(METcalc <= 14)
+            METmeasure = 6;
+        else // METcalc > 14
+            METmeasure = 5;
+        calories = (int)((double)METmeasure * 3.5 * ((double)user.getWeight()/2.2)/200);
+
+        return calories;
+    }
+
+}
